baef99d6d7512c2d07abcbd82e4b490c7e8e8968
Mavrick-DevOps <mavrick202@gmail.com>
2021-02-02 02:26:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 #This Terraform Code Deploys Basic VPC Infra.
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "sreeterraformbucket"
+    key    = "dockerslave.tfstate"
+    region = "us-east-1"
+  }
+}
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
@@ -99,23 +104,23 @@ resource "aws_security_group" "allow_all" {
 # }
 
 
-resource "aws_instance" "web-1" {
-    ami = var.imagename
-    #ami = "ami-0d857ff0f5fc4e03b"
-    #ami = "${data.aws_ami.my_ami.id}"
-    availability_zone = "us-east-1a"
-    instance_type = "t2.micro"
-    key_name = "LaptopKey"
-    subnet_id = "${aws_subnet.subnet1-public.id}"
-    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-    associate_public_ip_address = true	
-    tags = {
-        Name = "Server-1"
-        Env = "Prod"
-        Owner = "Sree"
-	CostCenter = "ABCD"
-    }
-}
+# resource "aws_instance" "web-1" {
+#     ami = var.imagename
+#     #ami = "ami-0d857ff0f5fc4e03b"
+#     #ami = "${data.aws_ami.my_ami.id}"
+#     availability_zone = "us-east-1a"
+#     instance_type = "t2.micro"
+#     key_name = "LaptopKey"
+#     subnet_id = "${aws_subnet.subnet1-public.id}"
+#     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+#     associate_public_ip_address = true	
+#     tags = {
+#         Name = "Server-1"
+#         Env = "Prod"
+#         Owner = "Sree"
+# 	CostCenter = "ABCD"
+#     }
+# }
 
 ##output "ami_id" {
 #  value = "${data.aws_ami.my_ami.id}"
